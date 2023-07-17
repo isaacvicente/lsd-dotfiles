81497cf895fa3010993c04841040f24d86ae9f31
Isaac Vicente <contatoisaacvicente@gmail.com>
2023-07-16 22:58:28 -0300

    Fix script

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 if command -v stow &> /dev/null
 then
-    stow --target=$HOME --dotfiles ../vim
+    stow --target=$HOME --dotfiles vim
 else
     echo "Stow is not installed."
 fi
